9db2edc40bfd97fbe3883f337c8e12d2a82b51f8
unknown <shubhamraj7352@gmail.com>
2025-03-07 19:40:54 +0530

    Added login and database schema

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { Button } from './components/ui/button'
+import Login from '@/pages/Login'
+
+function App() {
+  return (
+    <main>
+      <Login/>
+    </main>
+  )
+}
+
+export default App
---
+++ server/controllers/user.controller.js
@@ -0,0 +1,77 @@
+import { response } from "express";
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+
+export const register = async (req,res) => {
+    try{
+        const {name, email, password} = req.body; // shubham214
+        if(!name || !email || !password){
+            return res.status(400).json({
+                success: false,
+                message: "All fields are required."
+            })
+        }
+        const user = await User.findOne({email});
+        if(user){
+            return res.status(400).json({
+                success: false,
+                message: "User already exists with this email."
+            })
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10); // salt no=10 default
+        // like password dega vhygyty@4# kuchh bhi
+
+        // creaing user with registration.
+        await User.create({
+            name,
+            email,
+            password: hashedPassword
+        });
+        return res.status(201).json({
+            sucess: true,
+            message: "Account created successfully."
+        })
+    }
+    catch(error){
+        console.log(error);
+        return res.status(500).json({
+            sucess: false,
+            message: "Failed to register"
+        })
+    }
+}
+
+export const login = async (req,res) => {
+    try{
+        const {email, password} = req.body;
+        if(!email || !password){
+            return res.status(400).json({
+                success: false,
+                message: "All fields are required."
+            })
+        }
+        const user = await User.findOne({email}); // user ka mail check krne ke liye ki exist krta h nhi, for login
+        if(!user){
+            return res.status(400).json({
+                success: false,
+                message: "Incorrect email or password"
+            });
+        }
+        const isPasswordMatch = await bcrypt.compare(password, user.password);
+        if(!isPasswordMatch){
+            return res.status(400).json({
+                success: false,
+                message: "Incorrect email or password"
+            });
+        }
+
+    }
+    catch(error){
+        console.log(error);
+        return res.status(500).json({
+            sucess: false,
+            message: "Failed to login"
+        })
+    }
+}
