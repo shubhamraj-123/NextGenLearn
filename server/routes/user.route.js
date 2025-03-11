7a5bce90af4020d8a685a8268f7b5ba4941dce01
unknown <shubhamraj7352@gmail.com>
2025-03-11 20:00:37 +0530

    Updated user controller and routes, added middlewares

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,10 +1,13 @@
 // creating api, route create
 import express from "express";
-import { login, register } from "../controllers/user.controller.js";
+import { getUserProfile, login, logout, register } from "../controllers/user.controller.js";
+import isAuthenticated from "../middlewares/isAuthenticated.js";
 
 const router = express.Router();
 
 router.route("/register").post(register); // register import from user.controller.js
 router.route("/login").post(login);
+router.route("/logout").get(logout);
+router.route("/profile").get(isAuthenticated, getUserProfile);
 
 export default router;
