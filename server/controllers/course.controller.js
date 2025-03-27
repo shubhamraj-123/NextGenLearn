ed40674f094d43fa975053ebc3036758506c23cd
unknown <shubhamraj7352@gmail.com>
2025-03-28 02:18:00 +0530

    Update course controller and routes for improved functionality for creating &  getting course by id

--- server/controllers/course.controller.js
+++ server/controllers/course.controller.js
@@ -1,4 +1,5 @@
 import { Course } from "../models/course.model.js";
+import {deleteMediaFromCloudinary, uploadMedia} from "../utils/cloudinary.js"
 
 export const createCourse = async (req,res) => {
     try {
@@ -24,6 +25,90 @@ export const createCourse = async (req,res) => {
         console.log(error)
         return res.status(500).json({
             message:"Failed to create course"
+        })
+    }
+}
+
+// get course
+export const getCreatorCourses=async (req,res) => {
+    try {
+        const userId = req.id;
+        const courses = await Course.find({creator:userId});
+        if(!courses){
+            return res.status(404).json({
+                courses:[],
+                message:"Course not found"
+            })
+        };
+        return res.status(200).json({
+            courses,
+        })
+    } 
+    catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            message:"Failed to create course"
+        })
+
+    }
+}
+
+export const editCourse = async(req,res) => {
+    try {
+        const courseId = req.params.courseId;
+        const {courseTitle,subTitle,description,category,courseLevel,coursePrice} = req.body;
+        const thumbnail=req.file; // body se nhi file se lenge
+
+        let course = await Course.findById(courseId);
+        if(!course){
+            return res.status(404).json({
+                message:"Course not found"
+            })
+        }
+        let courseThumbnail;
+        if(thumbnail){ // after updation of course thumbnail then delete exists one
+            if(course.courseThumbnail){
+                const publicId = course.courseThumbnail.split("/").pop().split(".")[0]; // getting public Id
+                await deleteMediaFromCloudinary(publicId); // delete old image
+            }
+            // upload a thumbnail on cloudinary
+            courseThumbnail = await uploadMedia(thumbnail.path);
+        }
+
+        // updated data
+        const updateData = {courseTitle,subTitle,description,category,courseLevel,coursePrice,courseThumbnail:courseThumbnail?.secure_url};
+        course = await Course.findByIdAndUpdate(courseId,updateData,{new:true}) // updated data dega
+        return res.status(200).json({
+            course,
+            message:"Course updated successfully."
+        })
+    } 
+    catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            message:"Failed to create course"
+        })
+    }
+}
+
+export const getCourseById = async (req,res) => {
+    try {
+        const {courseId} = req.params;
+
+        const course = await Course.findById(courseId);
+        if(!course){
+            return res.status(404).json({
+                message:"Course not found!"
+            })
+        }
+        return res.status(200).json({
+            course
+        })
+    } 
+    catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            message:"Failed to get course by id"
         })
     }
 }
