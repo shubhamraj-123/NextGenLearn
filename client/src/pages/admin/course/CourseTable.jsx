bd27afaa88e6da62f2fd50f2a7d39b0005a568ec
unknown <shubhamraj7352@gmail.com>
2025-03-28 03:59:34 +0530

    Added CourseTab component and updated CourseTable functionality

--- client/src/pages/admin/course/CourseTable.jsx
+++ client/src/pages/admin/course/CourseTable.jsx
@@ -1,3 +1,4 @@
+import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import {
   Table,
@@ -9,6 +10,8 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import { useGetCreatorCourseQuery } from "@/features/api/courseApi";
+import { Edit } from "lucide-react";
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
@@ -58,12 +61,15 @@ const invoices = [
 ];
 
 const CourseTable = () => {
+  const {data, isLoading} = useGetCreatorCourseQuery();
     const navigate = useNavigate();
+    if(isLoading) return <h1>Loading...</h1>
+    // console.log("data -> ",data);
   return (
     <div>
       <Button onClick={() => navigate(`create`)}>Create a new Course</Button>
       <Table>
-        <TableCaption>A list of your recent invoices.</TableCaption>
+        <TableCaption>A list of your recent courses.</TableCaption>
         <TableHeader>
           <TableRow>
             <TableHead className="w-[100px]">Price</TableHead>
@@ -73,24 +79,25 @@ const CourseTable = () => {
           </TableRow>
         </TableHeader>
         <TableBody>
-          {invoices.map((invoice) => (
-            <TableRow key={invoice.invoice}>
-              <TableCell className="font-medium">{invoice.invoice}</TableCell>
-              <TableCell>{invoice.paymentStatus}</TableCell>
-              <TableCell>{invoice.paymentMethod}</TableCell>
+          {data.courses.map((course) => (
+            <TableRow key={course._id}>
+              <TableCell className="font-medium">{course?.coursePrice || "NA"}</TableCell>
+              <TableCell><Badge>{course.isPublished ? "Published" : "Draft"}</Badge></TableCell>
+              <TableCell>{course.courseTitle}</TableCell>
               <TableCell className="text-right">
-                {invoice.totalAmount}
+                <Button size='sm' variant='ghost' onClick={() => navigate(`${course._id}`)}><Edit/></Button>
               </TableCell>
             </TableRow>
           ))}
         </TableBody>
-        <TableFooter>
+        {/* <TableFooter>
           <TableRow>
             <TableCell colSpan={3}>Total</TableCell>
             <TableCell className="text-right">$2,500.00</TableCell>
           </TableRow>
-        </TableFooter>
+        </TableFooter> */}
       </Table>
+      
     </div>
   );
 };
