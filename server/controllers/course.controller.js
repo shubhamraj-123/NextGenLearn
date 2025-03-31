55f104fb3db7c4aec78fcedb1353d807281ea375
unknown <shubhamraj7352@gmail.com>
2025-03-31 16:23:53 +0530

    feat: Add route and controller to fetch published courses

--- server/controllers/course.controller.js
+++ server/controllers/course.controller.js
@@ -30,6 +30,27 @@ export const createCourse = async (req,res) => {
     }
 }
 
+// get published course in landing page
+export const getPublishedCourse = async(_,res) => {
+    try {
+        const courses = await Course.find({isPublished:true}).populate({path:"creator",select:"name photoUrl"});
+        if(!courses){
+            return res.status(404).json({
+                message:"Course not found"
+            })
+        }
+        return res.status(200).json({
+            courses,
+        })
+    } 
+    catch (error) {
+        console.log(error)
+        return res.status(500).json({
+            message:"Failed to get published courses"
+        })
+    }
+}
+
 // get course
 export const getCreatorCourses=async (req,res) => {
     try {
