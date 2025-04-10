a02912b570d8c5c714d59c73112864643c051a5e
unknown <shubhamraj7352@gmail.com>
2025-04-11 01:09:13 +0530

    Update SearchPage

--- client/src/pages/student/SearchPage.jsx
+++ client/src/pages/student/SearchPage.jsx
@@ -28,7 +28,7 @@ const SearchPage = () => {
   return (
     <div className="max-w-7xl mx-auto p-4 md:p-8">
       <div className="my-6">
-        <h1 className="font-bold text-xl md:text-2xl">Results for "{query}"</h1>
+        <h1 className="font-bold text-xl md:text-2xl">Results for "{query}" that you've searched</h1>
         {/* <p>
           Showing results for {}
           <span className="text-blue-800 font-bold italic">{query}</span>
