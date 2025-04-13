ebf319cb62d8bcd8c82d6eeca4f661d738597227
unknown <shubhamraj7352@gmail.com>
2025-04-13 23:08:01 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -26,8 +26,9 @@ app.use(cookieParser());
 // app.use(cors());
 app.use(cors(
    {
-    origin:"http://localhost:5173",
+    // origin:"http://localhost:5173",
     // origin:"https://next-gen-learn-jtda.vercel.app",
+    origin: ["http://localhost:5173", "https://next-gen-learn-jtda.vercel.app"],
     credentials:true
 }
 ))
