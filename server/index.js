10692858481f652651d61df46c9bf831edb2d8f8
unknown <shubhamraj7352@gmail.com>
2025-04-13 15:50:50 +0530

    Change in index.jsx

--- server/index.js
+++ server/index.js
@@ -23,11 +23,14 @@ const PORT=process.env.PORT || 3000;
 // default middleware
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
+app.use(cors());
+// app.use(cors(
+  //  {
     // origin:"http://localhost:8080",
     // origin:"http://localhost:5173",
     // credentials:true
-}))
+//}
+// ))
 
 // apis 
 // create ho rha like endpoint user
