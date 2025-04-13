ae4a99fe27e9f309478f27345f0272ef367ce2cb
unknown <shubhamraj7352@gmail.com>
2025-04-13 15:00:11 +0530

    chore: change index.js

--- server/index.js
+++ server/index.js
@@ -25,8 +25,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
     // origin:"http://localhost:8080",
-    origin:"http://localhost:5173",
-    credentials:true
+    // origin:"http://localhost:5173",
+    // credentials:true
 }))
 
 // apis 
